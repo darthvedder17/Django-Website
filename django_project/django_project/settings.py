bfe71a9c91bafb7d690ca3cb014f6012cbfbe277
Shaurya <darthvedder69@gmail.com>
2019-12-11 15:02:47 +0530

    Made changes to the Form using Crispy

--- django_project/django_project/settings.py
+++ django_project/django_project/settings.py
@@ -38,6 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'users.apps.UsersConfig',
+    'crispy_forms',
 ]
 
 MIDDLEWARE = [
@@ -119,3 +121,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+CRISPY_TEMPLATE_PACK = 'bootstrap4'

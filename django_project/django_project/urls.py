0912763a8ea78f4ae1ae4249b6853402ceed3a23
Shaurya <darthvedder69@gmail.com>
2019-12-11 17:23:18 +0530

    Added the Profile Picture

--- django_project/django_project/urls.py
+++ django_project/django_project/urls.py
@@ -3,7 +3,8 @@ from django.contrib import admin
 from django.contrib.auth import views as auth_views
 from django.urls import include,path
 from users import views as user_views
-
+from django.conf import settings
+from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('register/' ,user_views.register , name = 'register'),
@@ -13,3 +14,5 @@ urlpatterns = [
     path('logout/',auth_views.LogoutView.as_view(template_name = 'users/logout.html'),name='logout'),
 
 ]
+if settings.DEBUG:
+    urlpatterns+= static(settings.MEDIA_URL , document_root = settings.MEDIA_ROOT)

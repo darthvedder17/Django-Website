e48651c7444473e49f2456f1db2c1ece309c4691
Shaurya <darthvedder69@gmail.com>
2019-12-11 11:27:46 +0530

    Made Changes in the database

--- django_project/blog/models.py
+++ django_project/blog/models.py
@@ -10,4 +10,4 @@ class Post(models.Model):
     author = models.ForeignKey(User,on_delete=models.CASCADE)
 
     def __str__(self):
-        return  self.title
+        return self.title
--- django_project/blog/views.py
+++ django_project/blog/views.py
@@ -1,18 +1,6 @@
 from django.shortcuts import render
+from .models import Post
 
-posts = [
-    {'author' : 'CoreyMS' ,
-     'title' : 'Blog Post 1' ,
-     'content' : 'First post Content',
-     'date_posted' : 'August 27, 2018'
-
-    },
-    {'author': 'Janet Brown',
-     'title': 'Blog Post 2',
-     'content': 'Second post Content',
-     'date_posted': 'August 28, 2018'
-     },
-]
 
 
 
@@ -20,7 +8,7 @@ posts = [
 
 def home(request):
     context = {
-     'posts' : posts
+     'posts' : Post.objects.all()
         }
     return render(request , 'blog/home.html',context)
 

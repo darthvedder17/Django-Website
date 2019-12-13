8664752a9b5455871b1c54bb97d0f585ff524bda
Shaurya <darthvedder69@gmail.com>
2019-12-13 16:02:15 +0530

    An Exception in the TestUser Profile

--- django_project/blog/urls.py
+++ django_project/blog/urls.py
@@ -1,11 +1,15 @@
 from django.urls import path
 from . import views
-from django.contrib.staticfiles.urls import staticfiles_urlpatterns
-
+# from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+from .views import PostListView,PostDetailView,PostCreateView,PostUpdateView,PostDeleteView
 
 urlpatterns = [
     #Used to specify the home page
-    path('' , views.home , name = 'blog-home' ),
-    path('about/', views.about , name = 'blog-about')
+    path('' , PostListView.as_view() , name = 'blog-home' ),
+    path('post/new/', PostCreateView.as_view(), name='post-create'),
+    path('post/<int:pk>/', PostDetailView.as_view(), name='post-detail'),
+    path('about/', views.about , name = 'blog-about'),
+    path('post/<int:pk>/update/', PostUpdateView.as_view(), name='post-update'),
+    path('post/<int:pk>/delete/', PostDeleteView.as_view(), name='post-delete'),
 
 ]

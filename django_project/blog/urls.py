a3d342e92ad36864ae79c53f8f777b9ad823f7b0
Shaurya <darthvedder69@gmail.com>
2019-12-13 17:36:59 +0530

    Updated Website with Pagination and User Posted Profiles

--- django_project/blog/urls.py
+++ django_project/blog/urls.py
@@ -1,11 +1,13 @@
 from django.urls import path
 from . import views
 # from django.contrib.staticfiles.urls import staticfiles_urlpatterns
-from .views import PostListView,PostDetailView,PostCreateView,PostUpdateView,PostDeleteView
+from .views import PostListView,PostDetailView,PostCreateView,PostUpdateView,PostDeleteView,UserPostListView
 
 urlpatterns = [
     #Used to specify the home page
+
     path('' , PostListView.as_view() , name = 'blog-home' ),
+    path('user/<str:username>', UserPostListView.as_view(), name='user-posts'),
     path('post/new/', PostCreateView.as_view(), name='post-create'),
     path('post/<int:pk>/', PostDetailView.as_view(), name='post-detail'),
     path('about/', views.about , name = 'blog-about'),
